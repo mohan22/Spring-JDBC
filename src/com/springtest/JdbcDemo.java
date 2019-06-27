3e1a3ac874b2df9f77e96d2a120f5664601eca0e
mohan22 <jakkula4@gmail.com>
2019-06-27 15:21:55 +0530

    spring dao support

--- src/com/springtest/JdbcDemo.java
+++ src/com/springtest/JdbcDemo.java
@@ -3,17 +3,17 @@ package com.springtest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.springtest.dao.JdbcDaoImpl;
+import com.springtest.dao.SimpleJdbcDaoImpl;
 
 public class JdbcDemo {
 
 	public static void main(String[] args) {
 
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
-		JdbcDaoImpl dao = ctx.getBean("jdbcDaoImpl", JdbcDaoImpl.class);
+		SimpleJdbcDaoImpl dao = ctx.getBean("simpleJdbcDaoImpl", SimpleJdbcDaoImpl.class);
 		// dao.insert(new Circle(3, "second circle"));
 		// System.out.println(dao.getAllCircles());
-		dao.createTriangleTable();
+		System.out.println(dao.getCircleCount());
 	}
 
 }
---
+++ src/com/springtest/dao/SimpleJdbcDaoImpl.java
@@ -0,0 +1,10 @@
+package com.springtest.dao;
+
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+
+public class SimpleJdbcDaoImpl extends JdbcDaoSupport {
+	public int getCircleCount() {
+		String sql = "select count(*) from circle";
+		return this.getJdbcTemplate().queryForObject(sql, Integer.class);
+	}
+}
